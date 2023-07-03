765a00c5cae9f0d3564c6855c4f889f0dff509a7
sampath-kumaramd <pmmdskk@gmail.com>
2023-07-04 05:12:18 +0530

    bugs fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <MainNav className='mx-6'/>
-        <div className="ml-auto flex items-center space-x-4"><UserButton afterSignOutUrl="http://localhost:3000/"/></div>
+        <div className="ml-auto flex items-center space-x-4"><UserButton afterSignOutUrl="/"/></div>
       </div>
     </div>
   );
